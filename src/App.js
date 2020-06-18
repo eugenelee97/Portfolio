cda07fbf10faa626d464f9bf2998a4345d702dc2
eugenelee97 <pinkfjgh@gmail.com>
2020-06-19 04:04:09 +0900

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useRef } from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { eugeneTheme } from "./component/theme/theme_edit";
-import "./App.css";
 import Nav from "./component/nav/Nav";
 import StickyFooter from "./component/footer/StickyFooter";
 import AboutMe from "./component/aboutMe/AboutMe";
