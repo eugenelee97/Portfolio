9f9b1c5ca370b3f61b5bb11e0af8260e6ba5a78d
eugenelee97 <pinkfjgh@gmail.com>
2020-06-20 00:47:17 +0900

    added grid md

--- src/component/aboutMe/About_Main.js
+++ src/component/aboutMe/About_Main.js
@@ -25,7 +25,7 @@ export default function AboutTxt() {
   const classes = useStyles();
 
   return (
-    <Grid item sm={12}>
+    <Grid item sm={12} md={6}>
       <Typography variant="h3" component="h1">
         About <span className={classes.other}>Me</span>
       </Typography>
